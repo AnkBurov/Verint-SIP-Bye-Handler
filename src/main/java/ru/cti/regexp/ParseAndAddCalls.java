836afa97432206cc307b767e5acb4c9eb09b7850
Ank Burov <michmanharitonov1@gmail.com>
2016-02-26 22:48:06 +0300

    fix pattern place

--- src/main/java/ru/cti/regexp/ParseAndAddCalls.java
+++ src/main/java/ru/cti/regexp/ParseAndAddCalls.java
@@ -21,6 +21,7 @@ public class ParseAndAddCalls {
         /*директория для парсинга логов*/
         File dir = new File("C:\\drivers\\");
         File[] files = dir.listFiles((d, name) -> name.endsWith(".log"));
+        Pattern pattern = Pattern.compile("[a-f0-9-]{30,40}\\@(?:\\d{1,3}\\.){3}\\d{1,3}");
         long before = System.currentTimeMillis();
         for (File file : files) {
             System.out.println(file.getAbsolutePath());
@@ -29,16 +30,12 @@ public class ParseAndAddCalls {
             FileReader fileReader = null;
             try {
                 fileReader = new FileReader(file);
-//                char[] buffer = new char[(int) file.length()];
                 // считаем файл полностью
-//                fileReader.read(buffer);
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
                 String buffer;
-                // todo итератор?
                 while ((buffer = bufferedReader.readLine()) != null) {
                     if (buffer.length() > 200) {
                         if (buffer.contains("Request<INVITE>")) {
-                            Pattern pattern = Pattern.compile("[a-f0-9-]{30,40}\\@(?:\\d{1,3}\\.){3}\\d{1,3}");
                             Matcher matcher = pattern.matcher(buffer.substring(200));
                             if (matcher.find()) {
                                 calls.add(new Call(System.currentTimeMillis(), matcher.group()));
